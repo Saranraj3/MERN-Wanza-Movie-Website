89a9a7b47b8abb8afed1e2a4bcac1d9ba2211998
Saranraj <www.saranrajcoder@gmail.com>
2024-03-05 08:21:29 +0530

    Navbar Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <div >App</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from '../../Assets/Logo/film.png'
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import PersonIcon from '@mui/icons-material/Person';
+import MenuIcon from '@mui/icons-material/Menu';
+function Navbar() {
+
+    return (
+        <div className='h-12 pt-2 bg-black '>
+            <img className='h-7 ml-4 cursor-pointer' src={Logo} alt="" />
+            <h1 className='text-white ml-16 mt-[-1.5rem] cursor-pointer'>Wanza</h1>
+            <ul className=' text-white text-xl mr-5 flex gap-10 mt-[-1.7rem] cursor-pointer justify-center font-serif'>
+                <li>Home</li>
+                <li>Movies</li>
+                <li>Shows</li>
+                <li>About</li>
+            </ul>
+            <div className='flex gap-4 justify-end mr-10 mt-[-1.7rem]'>
+                <p className='text-white'><SearchIcon /></p>
+                <p className='text-white'><FavoriteOutlinedIcon /></p>
+                <p className='text-white'><PersonIcon /></p>
+                <p className='text-white'><MenuIcon /></p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
