d169dca3c3986c377b0c0e5f8fa5e110b316b9ec
Saranraj <www.saranrajcoder@gmail.com>
2024-03-08 14:40:47 +0530

    Footer Page Modified Background Image Removed

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import footer from '../../Assets/Logo/footer.jpg'
+import Logo from '../../Assets/Logo/film.png'
+
 function Footer() {
   return (
-    <div className='mt-20 absolute'>
-      <img className='opacity-20 relative' src={footer} alt="" />
-      <h1 className='mt-[-25rem] ml-[6rem] text-xl font-serif font-semibold  text-white '>INFORM</h1>
-      <p className=' mt-[2rem] ml-[6rem] text-lg font-extrabold text-white'>Home</p>
-      <p className=' mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>Contact Us</p>
-      <p className=' mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>Terms Of Service</p>
-      <p className=' mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>About Us</p>
-      
-      <h1 className='mt-[-13.8rem] ml-[25rem] text-xl font-serif font-semibold  text-white '>RESOURCES</h1>
-      <p className=' mt-[2rem] ml-[25rem] text-lg font-extrabold text-white'>Live</p>
-      <p className=' mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>Premium</p>
-      <p className=' mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>FAQ</p>
-      <p className=' mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>Privacy Policy</p>
+    <div className=' mt-[40rem]  '>
 
-      <h1 className='mt-[-13.8rem] ml-[45rem] text-xl font-serif font-semibold  text-white '>CATEGORYS</h1>
-      <p className=' mt-[2rem] ml-[45rem] text-lg font-extrabold text-white'>Movies</p>
-      <p className=' mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>Tv Shows</p>
-      <p className=' mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>API</p>
-      <p className=' mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>Developer</p>
+        <h1 className='cursor-pointer  mt-[-25rem] ml-[6rem] text-xl font-serif font-semibold text-white '>INFORM</h1>
+        <p className='cursor-pointer  mt-[2rem] ml-[6rem] text-lg font-extrabold text-white'>Home</p>
+        <p className='cursor-pointer mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>Contact Us</p>
+        <p className='cursor-pointer mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>Terms Of Service</p>
+        <p className='cursor-pointer mt-[1rem] ml-[6rem] text-lg font-extrabold text-white'>About Us</p>
 
-    </div>
+        <h1 className='cursor-pointer mt-[-13.8rem] ml-[25rem] text-xl font-serif font-semibold  text-white '>RESOURCES</h1>
+        <p className=' cursor-pointer mt-[2rem] ml-[25rem] text-lg font-extrabold text-white'>Live</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>Premium</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>FAQ</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[25rem] text-lg font-extrabold text-white'>Privacy Policy</p>
+
+        <h1 className='cursor-pointer mt-[-13.8rem] ml-[45rem] text-xl font-serif font-semibold text-white '>CATEGORYS</h1>
+        <p className=' cursor-pointer mt-[2rem] ml-[45rem] text-lg font-extrabold text-white'>Movies</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>Tv Shows</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>API</p>
+        <p className=' cursor-pointer mt-[1rem] ml-[45rem] text-lg font-extrabold text-white'>Developer</p>
+        <img className='h-10 ml-[60rem] cursor-pointer' src={Logo} alt="" />
+        <h1 className='text-white mt-[-2rem] ml-[63rem] '>Wanza</h1>
+      </div>
   )
 }
 
